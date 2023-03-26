f80c19a74338a799ab791534d0ea44356669fe86
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2023-03-26 14:38:13 +0200

    Create script for app

---
+++ public/script.js
@@ -0,0 +1,93 @@
+const map = L.map('map');
+const submitBtn = document.querySelector('button[type="submit"]');
+
+showData();
+
+submitBtn.addEventListener('click', (event) => {
+    showLoading(true);
+    searchAddress();
+    event.preventDefault();
+})
+
+function searchAddress() {
+    const ipAddressInput = document.querySelector('#ip-address-input');
+    const address = ipAddressInput.value;
+
+    if (isAddressValid(address)) {
+        showData(address);
+        showError(false);
+    } else {
+        showLoading(false);
+        showError(true);
+    }
+}
+
+async function getData(address) {
+
+    try {
+        const response = await fetch(`/api/${address}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function showData(address) {
+
+    const data = await getData(address);
+
+    if (data.code === 400) {
+        showError(true, 'Domain not found');
+        showLoading(false);
+        return;
+    }
+
+    const ipAddressSpan = document.querySelector('#ip-address');
+    const locationSpan = document.querySelector('#location');
+    const timezoneSpan = document.querySelector('#timezone');
+    const ispSpan = document.querySelector('#isp');
+    const { lat, lng, postalCode, city, timezone } = data.location;
+    const postalCodeInfo = postalCode === '' ? '' : `, ${postalCode}`;
+
+    ipAddressSpan.textContent = data.ip;
+    locationSpan.textContent = city + postalCodeInfo;
+    timezoneSpan.textContent = `UTC ${timezone}`;
+    ispSpan.textContent = data.isp;
+
+    console.log(data);
+    showLocationOnMap(lat, lng);
+    showLoading(false);
+}
+
+function showLocationOnMap(lat, lng) {
+    map.setView([lat, lng], 17);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+}
+
+function isAddressValid(address) {
+    const domainPattern = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/;
+    const ipPattern = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/;
+
+    return Boolean(
+        address.match(domainPattern) ||
+        address.match(ipPattern)
+    );
+}
+
+function showLoading(isLoad) {
+    const curtain = document.querySelector('.curtain');
+    curtain.style.display = isLoad ? "flex" : "none";
+}
+
+function showError(isError, errorMessage) {
+    const error = document.querySelector('.error');
+    error.textContent = errorMessage || 'Invalid IP address or domain!';
+    error.style.display = isError ? "block" : "none";
+}
