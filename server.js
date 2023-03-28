3f2968cb33df5142a496bfdeed9d76bd6e524f86
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2023-03-28 18:19:02 +0200

    Fix issue with ip (TEST)

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const port = process.env.PORT || 8888;
 const app = express();
 
 app.use(express.static('public'));
-
+app.set('trust proxy', true)
 app.listen(port, () => {
     console.log(`Starting server at port: ${port}`);
 })
-
 app.get('/api/:address', async (request, response) => {
     const apiKey = process.env.APIKEY;
     const address = request.params.address;
     const param = address !== 'undefined'
         ? `&domain=${address}`
-        : '';
+        : `&domain=${request.ip}`;
     const apiURL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}${param}`
     const fetchApi = await fetch(apiURL);
     const data = await fetchApi.json();
